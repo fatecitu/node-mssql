7ddc06df83b94b9952fa47325256058e53daa3e8
marcioferraz81 <marcio.ferraz@etec.sp.gov.br>
2023-03-27 12:38:18 -0300

    Aula 27/03

--- src/routes/veiculos.js
+++ src/routes/veiculos.js
@@ -1,6 +1,6 @@
 // API REST dos veículos
 import express from 'express'
-import sql from 'mssql'
+import sql, { pool } from 'mssql'
 import { sqlConfig } from '../sql/config.js'
 
 const router = express.Router()
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
     try {
         sql.connect(sqlConfig).then(pool => {
             return pool.request()
-            .execute('SP_S_EST_VEICULO')
+                .execute('SP_S_EST_VEICULO')
         }).then(dados => {
             res.status(200).json(dados.recordset)
         }).catch(err => {
@@ -21,5 +21,65 @@ router.get('/', (req, res) => {
     } catch (err) {
         console.error(`Erro ao conectar: ${err.message}`)
     }
+})
+/***************************************
+ * POST /veiculos
+ * Insere um novo veículo
+ ***************************************/
+router.post('/', (req, res) => {
+    sql.connect(sqlConfig).then(pool => {
+        const { placa, nome, descricao, fabricacao, preco } =
+            req.body
+        return pool.request()
+            .input('PLACA', sql.Char(7), placa)
+            .input('NOME', sql.VarChar(50), nome)
+            .input('DESCRICAO', sql.VarChar(200), descricao)
+            .input('FABRICACAO', sql.Date, fabricacao)
+            .input('PRECO', sql.Numeric, preco)
+            .output('CODIGOGERADO', sql.Int)
+            .execute('SP_I_EST_VEICULO')
+    }).then(dados => {
+        res.status(200).json(dados.output)
+    }).catch(err => {
+        res.status(400).json(err.message)
+    })
+})
+
+/***************************************
+ * PUT /veiculos
+ * Alterar um veículo existente
+ ***************************************/
+router.put("/", (req, res) => {
+    sql.connect(sqlConfig).then(pool => {
+        const { codigo, placa, nome, descricao, fabricacao, preco } = req.body
+        return pool.request()
+            .input('CODIGO', sql.Int, codigo)
+            .input('PLACA', sql.Char(7), placa)
+            .input('NOME', sql.VarChar(50), nome)
+            .input('DESCRICAO', sql.VarChar(200), descricao)
+            .input('FABRICACAO', sql.Date, fabricacao)
+            .input('PRECO', sql.Numeric, preco)
+            .execute('SP_U_EST_VEICULO')
+    }).then(dados => {
+        res.status(200).json(`Veículo ${placa} alterado com sucesso`)
+    }).catch(err => {
+        res.status(400).json(err.message)
+    })
+})
+/***************************************
+ * DELETE /veiculos
+ * Remove um veículo pela placa
+ ***************************************/
+router.delete(':/placa', (req, res) => {
+    sql.connect(sqlConfig).then(pool => {
+        const placa = req.params.placa
+        return pool.request()
+        .input('PLACA', sql.Char(7), placa)
+        .execute('SP_D_EST_VEICULO')
+    }).then(dados => {
+        res.status(200).json(`Veículo ${placa} removido com sucesso`)
+    }).catch(err => {
+        res.status(400).json(err.message)
+    })
 })
 export default router
--- src/sql/config.js
+++ src/sql/config.js
@@ -5,13 +5,9 @@ export const sqlConfig = {
     password: process.env.DB_PWD,
     database: process.env.DB_NAME,
     server: process.env.DB_SERVER,
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
-    },
+    port: 1434,    
     options: {
-        encrypt: false, //apenas para o Azure
-        trustServerCertificate: true //Mantenha para server local
+       trustedConnection: true,
+       enableArithAbort: true
     }
 }
