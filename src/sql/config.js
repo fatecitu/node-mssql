b090119ab88c5c91f32f8086fed47ce8807bfa59
marcioferraz81 <marcio.ferraz@etec.sp.gov.br>
2023-04-03 11:45:58 -0300

    Ajustes no config para o SQLEXPRESS

--- src/sql/config.js
+++ src/sql/config.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv/config'
 export const sqlConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
-  server: 'localhost\\SQLEXPRESS',
-  database: 'Estacionamento',
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
   port: 1433,
   options: {
       encrypt: true,
