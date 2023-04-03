2909337be6645589b28faf3a5101ae2b98b486c6
marcioferraz81 <marcio.ferraz@etec.sp.gov.br>
2023-04-03 11:12:23 -0300

    Atualização SQLEXPRESS

--- src/sql/config.js
+++ src/sql/config.js
@@ -1,17 +1,14 @@
 import dotenv from 'dotenv/config'
 
 export const sqlConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    server: process.env.DB_SERVER,
-    pool: {
-      max: 10,
-      min: 0,
-      idleTimeoutMillis: 30000
-    },
-    options: {
-      encrypt: false, // Apenas para o Azure
-      trustServerCertificate: true // Mantenha true apenas para servidores locais
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  server: 'localhost\\SQLEXPRESS',
+  database: 'Estacionamento',
+  port: 1433,
+  options: {
+      encrypt: true,
+      trustServerCertificate: true,
+      ssl: false // Desabilita a validação do certificado SSL
     }
-  }
+}
